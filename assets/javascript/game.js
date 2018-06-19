2846f1ff3ab87e888b375a1652749202cba1c8ca
Taylor Aanenson <tayloraanenson@Taylors-MacBook-Pro.local>
2018-06-18 18:53:27 -0700

    finished js and updated styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,40 +7,46 @@ var silverRupeeNum = (Math.floor(Math.random()*(12-1)+1));
 var wins = 0;
 var losses = 0;
 var counter = 0;
-console.log(randomNumber);
-console.log(counter);
 
 $('#randomNumber').text(randomNumber);
 $('#wins').text('Wins: ' + wins);
 $('#losses').text('Losses: ' + losses);
 $('#counter').text(counter);
 
-$('#greenRupee').on('click',function(){
-    counter+=greenRupeeNum;
+$('.crystals').click(function(){
+    var rupee = $(this).data('rupee');
+    if (rupee == 'green'){
+        counter+=greenRupeeNum;
+        $('#counter').text(counter);
+    }else if (rupee == 'blue'){
+        counter+=blueRupeeNum;
+        $('#counter').text(counter);
+    }else if (rupee == 'purple'){
+        counter+=purpleRupeeNum;
+        $('#counter').text(counter);
+    }else if (rupee == 'silver'){
+        counter+=silverRupeeNum;
+        $('#counter').text(counter);
+    }
+    if (counter === randomNumber){
+        wins++;
+        counter = 0;
+        randomNumber = (Math.floor(Math.random()*(120-19)+19));
+        greenRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        blueRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        purpleRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        silverRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+    }else if (counter > randomNumber){
+        losses++;
+        counter = 0;
+        randomNumber = (Math.floor(Math.random()*(120-19)+19));
+        greenRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        blueRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        purpleRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+        silverRupeeNum = (Math.floor(Math.random()*(12-1)+1));
+    }
+    $('#randomNumber').text(randomNumber);
+    $('#wins').text('Wins: ' + wins);
+    $('#losses').text('Losses: ' + losses);
     $('#counter').text(counter);
-    console.log(greenRupeeNum);
-    console.log(counter);
 });
-$('#blueRupee').on('click',function(){
-    counter+=blueRupeeNum;
-    $('#counter').text(counter);
-    console.log(blueRupeeNum);
-    console.log(counter);
-});
-$('#purpleRupee').on('click',function(){
-    counter+=purpleRupeeNum;
-    $('#counter').text(counter);
-    console.log(purpleRupeeNum);
-    console.log(counter);
-});
-$('#silverRupee').on('click',function(){
-    counter+=silverRupeeNum;
-    $('#counter').text(counter);
-    console.log(silverRupeeNum);
-    console.log(counter);
-});
-if(randomNumber == counter){
-    alert('you win!');
-}else{
-    alert('nice try');
-}
